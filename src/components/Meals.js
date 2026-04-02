b3e158a7339a556f678ad086c8a4236beac3d351
Roven <roven.piho@voco.ee>
2026-04-02 11:26:43 +0300

    ulesanne1

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,11 +1,8 @@
-const Meals = () => {
-    return (
+return (
         <ul id="meals">
             { 
-                // list of meals
+                
+              <li><strong>create list of meals, using fetch data from backend</strong></li>  // list of meals
             }
         </ul>
     )
-}
-
-export default Meals
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Header from '../src/components/Header';
+import Meals from '../src/components/Meals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
+    <Meals />
     <App />
   </React.StrictMode>
 );
