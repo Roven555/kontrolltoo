8c5be4bab5b8e314036c4d2719a162db9fc8be1c
Roven <roven.piho@voco.ee>
2026-04-02 11:59:50 +0300

    Alustasin ülesanne 1 uuesti, olid errorid, sai valmis

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,8 +1,8 @@
-return (
-        <ul id="meals">
-            { 
-                
-              <li><strong>create list of meals, using fetch data from backend</strong></li>  // list of meals
-            }
-        </ul>
-    )
+export default function Meals() {
+
+  return (
+    <ul id="meals">
+      { } kkod
+    </ul>
+  );
+}
