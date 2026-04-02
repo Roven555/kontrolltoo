45efd838a159729dd3ffb774bd572ac866daeb97
Roven <roven.piho@voco.ee>
2026-04-02 14:46:48 +0300

    Ulesanne 7

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,41 +1,43 @@
 import { useEffect, useState } from "react";
-import MealItem from "./MealItem.js";
-import "../index.css";
+import MealItem from './MealItem.js'
+import '../index.css'
 
 const Meals = () => {
-  const [meals, setMeals] = useState([]);
+    const [meals, setMeals] = useState([])
 
-  useEffect(() => {
-    const fetchMeals = async () => {
-      try {
-        const response = await fetch("http://localhost:3001/meals");
-        if (!response.ok) {
-          throw new Error("Failed to fetch meals.");
+    useEffect(() => {
+        const fetchMeals = async () => {
+            try {
+                // Tuleb paring backendist
+                const response = await fetch("http://localhost:3001/meals") 
+                if (!response.ok) {
+                    throw new Error("Failed to fetch meals.")
+                }
+                const data = await response.json() 
+                console.log(data)
+                setMeals(data)
+            } catch (error) {
+                console.error("Error fetching meals:", error)
+            }
         }
-        const data = await response.json();
-        console.log(data);
-        setMeals(data);
-      } catch (error) {
-        console.error("Error fetching meals:", error);
-      }
-    };
 
-    fetchMeals();
-  }, []);
+        fetchMeals()
+    }, [])
 
-  return (
-    <ul id="meals">
-      {meals.map((meal) => (
-        <MealItem
-          key={meal.id}
-          name={meal.name}
-          price={meal.price}
-          description={meal.description}
-          image={meal.image}
-        />
-      ))}
-    </ul>
-  );
-};
+    return (
+        <ul id="meals">
+          {meals.map((meal) => (
+            <MealItem
+              key={meal.id}
+                id={meal.id}
+              name={meal.name}
+              price={meal.price}
+              description={meal.description}
+              image={meal.image}
+            />
+          ))}
+        </ul>
+      );
+}
 
-export default Meals;
+export default Meals
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Header from '../src/components/Header';
-import Meals from '../src/components/Meals'
-import React from 'react';
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import React from 'react'
 
-// Google ajas segadusse (;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Header />
-    <Meals />
     <App />
   </React.StrictMode>
-);
+)
