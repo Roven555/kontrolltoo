99689477dcffe22b33700a382a0843522e573b5a
Roven <roven.piho@voco.ee>
2026-04-02 13:50:01 +0300

    Ulesanne 5

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './index.css';
 import App from './App';
 import Header from '../src/components/Header';
 import Meals from '../src/components/Meals'
+import React from 'react';
+// Google ajas segadusse (;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
